2680d27b7007dfca6852aaede69fb3647c556b8f
Simon Giesecke <simon.giesecke@btc-ag.com>
2019-02-18 05:01:17 -0500

    Add build using conan workspace

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+set -e
+
+echo CONAN_USE_WORKSPACE=$CONAN_USE_WORKSPACE
+if [ "$CONAN_USE_WORKSPACE" == "false" ] ; then
+  conan create base foo/bar
+  conan create dependent foo/bar
+fi
+if [ "$CONAN_USE_WORKSPACE" == "true" ] ; then
+  conan install . -if=build
+  cd build
+  ${CONAN_CMAKE_PROGRAM} ..
+  ${CONAN_CMAKE_PROGRAM} --build .
+fi
+
